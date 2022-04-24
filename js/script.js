1c208ff7d3c6e7525e293cfd9c4f1874eb28bada
Anastasia Rarova <07tangled07@gmail.com>
2022-04-24 22:21:09 +0300

    a page up button added and styled + added a script for its smoothscrolling

--- js/script.js
+++ js/script.js
@@ -118,4 +118,22 @@ $(document).ready(function () {
         });
         return false;
     });
+
+    // Smooth scrolling and page up
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+      } else {
+          $('.pageup').fadeOut();
+      }
+  });
+
+  $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
+  new WOW().init();
+
 });
